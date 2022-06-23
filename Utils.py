837da4b3c42f469efa7fb5c03e25ea710091c558
Yash Eksambekar <yasheksambekar@gmail.com>
2022-06-23 10:27:51 +0530

    Database reset util added

--- Utils.py
+++ Utils.py
@@ -1,4 +1,27 @@
 from math import sin, cos, sqrt, atan2, radians
+import psycopg
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+conn_dict = psycopg.conninfo.conninfo_to_dict(os.getenv("DATABASE_URL"))
+
+
+conn = psycopg.connect(**conn_dict)
+
+mycursor = conn.cursor()
+
+
+def resetDB():
+    mycursor.execute("DROP TABLE images,donations, users")
+    conn.commit()
+    mycursor.execute("CREATE TABLE users(userId int unsigned NOT NULL UNIQUE,name varchar(35) NOT NULL,email varchar(50) NOT NULL UNIQUE,password varchar(32) NOT NULL,mobile varchar(10) NOT NULL,latitude float,longitude float,role varchar(10) NOT NULL,PRIMARY KEY (userId))")
+    conn.commit()
+    mycursor.execute("CREATE TABLE donations(donationId int unsigned NOT NULL UNIQUE,donoruserId int unsigned NOT NULL,description varchar(300) NOT NULL,inventory int unsigned NOT NULL,donationDate datetime NOT NULL,recieveruserId int unsigned,acceptationDate datetime,donationStatus char(10) NOT NULL,PRIMARY KEY (donationId),FOREIGN KEY (donoruserId) REFERENCES users(userId),FOREIGN KEY (recieveruserId) REFERENCES users(userId))")
+    conn.commit()
+    mycursor.execute("CREATE TABLE images(imageId int unsigned NOT NULL UNIQUE,donationId int unsigned NOT NULL,image mediumblob NOT NULL,PRIMARY KEY (imageId),FOREIGN KEY (donationId) REFERENCES donations(donationId))")
+    conn.commit()
 
 
 def calcDistance(p1, p2):
